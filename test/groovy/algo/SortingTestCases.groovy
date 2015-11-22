3dcd4696e4be79255a1edcad3cff12bdbea52830
Andrey Tsarevskiy <andreytsarevskiy@gmail.com>
2015-11-22 13:30:16 +0100

    merge sort implementation

--- test/groovy/algo/SortingTestCases.groovy
+++ test/groovy/algo/SortingTestCases.groovy
@@ -15,7 +15,6 @@ class SortingTestCases extends GroovyTestCase {
     void testInsertionSort() {
         int[] array = [5,2,3,6,3,2,4,10,2,3,5,3,4,7,8,60]
         Sorting.insertion(array)
-        print array
         int[] sortedArray = [2,2,2,3,3,3,3,4,4,5,5,6,7,8,10,60]
         assertTrue(array.equals(sortedArray))
     }
@@ -23,6 +22,13 @@ class SortingTestCases extends GroovyTestCase {
     void testSelectionSort() {
         int[] array = [5,2,3,6,3,2,4,10,2,3,5,3,4,7,8,60]
         Sorting.selection(array)
+        int[] sortedArray = [2,2,2,3,3,3,3,4,4,5,5,6,7,8,10,60]
+        assertTrue(array.equals(sortedArray))
+    }
+
+    void testMergeSort() {
+        int[] array = [5,2,3,6,3,2,4,10,2,3,5,3,4,7,8,60]
+        MergeSort.sort(array)
         print array
         int[] sortedArray = [2,2,2,3,3,3,3,4,4,5,5,6,7,8,10,60]
         assertTrue(array.equals(sortedArray))
