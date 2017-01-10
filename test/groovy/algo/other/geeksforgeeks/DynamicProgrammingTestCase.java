a51eba4b39ae69ca77500b55668feb49e29a9e1d
daxzel <andreytsarevskiy@gmail.com>
2017-01-10 01:17:27 +0100

    cracking the coding interview

---
+++ test/groovy/algo/other/geeksforgeeks/DynamicProgrammingTestCase.java
@@ -0,0 +1,19 @@
+package algo.other.geeksforgeeks;
+
+import algo.others.geeksforgeeks.DynamicProgramming;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by andrey tsarevskiy
+ */
+public class DynamicProgrammingTestCase extends Assert {
+
+    @Test
+    public void matrixChainOrder() {
+        int arr[] = new int[] {1, 2, 3, 4, 3};
+        int result = DynamicProgramming.matrixChainOrder(arr);
+        assertEquals(result, 86);
+    }
+
+}
