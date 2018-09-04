d45a0dd2e7f363b334364d2910d9028cd79be38f
Andrey Tsarevskiy <atcarevs@amazon.com>
2018-09-04 21:33:38 +0100

    Simple string matching

---
+++ src/groovy/algo/algorithm/string/SimpleMatching.java
@@ -0,0 +1,28 @@
+package algo.algorithm.string;
+
+public class SimpleMatching {
+
+    public static int find(char[] word, char[] text) {
+
+        for (int i = 0; i < text.length; i++) {
+
+            int j = 0;
+            char t = text[i];
+            char w = word[j];
+
+            if (t == w) {
+                j++;
+                while (t == w && j + i < text.length && j < word.length) {
+                    t = text[i + j];
+                    w = word[j];
+                    j++;
+                }
+                if (j == word.length && t == w) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+}
