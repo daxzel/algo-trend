b1a7e9d29c4b3a277763d5e3ef737d8f4fcc16ce
Andrey Tsarevskiy <atcarevs@amazon.com>
2021-03-17 23:05:05 +0000

    Principal_component_analysis

--- src/go/algo/algorithm/math/pca.go
+++ src/go/algo/algorithm/math/pca.go
@@ -88,12 +88,159 @@ func Matrix2String(data [][]float64) string {
 	return s
 }
 
-func main() {
-	input := [][]float64{
-		{0, 1, 2, 3},
-		{0, 1, 4, 9},
-		{3, 2, 1, 1},
+func Vector2String(data []float64) string {
+	s := ""
+	for _, n := range data {
+		s += fmt.Sprintf("%f | ", n)
+	}
+	return s
+}
+
+func DotProduct(x []float64, y []float64) float64 {
+	result := 0.0
+	for j := 0; j < len(x); j++ {
+		result += x[j] * y[j]
+	}
+	return result
+}
+
+func Minus(x []float64, y []float64) []float64 {
+	result := make([]float64, len(x))
+	for j := 0; j < len(x); j++ {
+		result[j] = x[j] - y[j]
+	}
+	return result
+}
+
+func Power(x []float64, power float64) []float64 {
+	result := make([]float64, len(x))
+	for j := 0; j < len(x); j++ {
+		result[j] = math.Pow(x[j], power)
+	}
+	return result
+}
+
+func Sum(x []float64) float64 {
+	result := 0.0
+	for j := 0; j < len(x); j++ {
+		result += x[j]
+	}
+	return result
+}
+
+func GetColumn(x [][]float64, column int) []float64 {
+	result := make([]float64, len(x))
+	for j := 0; j < len(x); j++ {
+		result[j] = x[j][column]
+	}
+	return result
+}
+
+func DotProductMatrix(x [][]float64, y [][]float64) [][]float64 {
+	result := make([][]float64, len(x))
+	for i := 0; i < len(x); i++ {
+		result[i] = make([]float64, len(y))
+		for j := 0; j < len(y[0]); j++ {
+			result[i][j] = DotProduct(x[i], GetColumn(y, j))
+		}
+	}
+	return result
+}
+
+func NormalizeVector(x []float64) [] float64 {
+	norm := math.Pow(DotProduct(x, x), 0.5)
+
+	for j := 0; j < len(x); j++ {
+		x[j] = x[j] / norm
+	}
+	return x
+}
+
+func Copy(data [][]float64) [][]float64 {
+	duplicate := make([][]float64, len(data))
+	for i := range data {
+		duplicate[i] = make([]float64, len(data[i]))
+		copy(duplicate[i], data[i])
+	}
+	return duplicate
+}
+
+/* Find an orthonormal basis for the set of vectors q
+ * using the Gram-Schmidt Orthogonalization process */
+func GramSchimdt(data [][]float64) [][]float64 {
+	result := Copy(data)
+	for i := 1; i < len(result); i++ {
+		for j := 0; j < i; j++ {
+			factor := DotProduct(result[j], result[i]) / DotProduct(result[j], result[j])
+			/* Subtract each scaled component of q_j from q_i */
+			for k := 0; k < len(result); k++ {
+				result[i][k] -= factor * result[j][k]
+			}
+		}
 	}
 
-	fmt.Print(Matrix2String(CovMatrix(Normalize(input))))
+	for i := 0; i < len(result); i++ {
+		result[i] = NormalizeVector(result[i])
+	}
+	return result
+}
+
+func Transpose(data [][]float64) [][]float64 {
+	result := Copy(data)
+	for i := 0; i < len(result); i++ {
+		for j := i; j < len(result); j++ {
+			temp := result[i][j]
+			result[i][j] = result[j][i]
+			result[j][i] = temp
+		}
+	}
+	return result
+}
+
+func Diag(data [][]float64) []float64 {
+	result := make([]float64, len(data))
+	for i := 0; i < len(result); i++ {
+		result[i] = data[i][i]
+	}
+	return result
+}
+
+type EigenSummary struct {
+	EigenVectors [][]float64
+	EigenValues []float64
+}
+
+func Eigen(data [][]float64) EigenSummary {
+	Q := Transpose(GramSchimdt(data))
+	E := DotProductMatrix(DotProductMatrix(Transpose(Q), data), Q)
+	U := Q
+	result := Diag(E)
+	init := Diag(data)
+	for Sum(Power(Minus(init, result), 2.0)) > 1e-20 {
+		init = result
+		Q = Transpose(GramSchimdt(E))
+		E = DotProductMatrix(DotProductMatrix(Transpose(Q), E), Q)
+		U = DotProductMatrix(U, Q)
+		result = Diag(E)
+	}
+	return EigenSummary{U, result}
+}
+
+func main() {
+
+	// https://en.wikipedia.org/wiki/Principal_component_analysis
+
+	input2 := [][]float64{
+		{1, 2, 4, 6},
+		{1, 6, 7, 8},
+		{1, 1, 1, 10},
+		{4, 2, 1, 1},
+	}
+
+	E := Eigen(CovMatrix(Normalize(input2)))
+
+	fmt.Print(Matrix2String(E.EigenVectors))
+	fmt.Print("\n----------------------\n")
+	fmt.Print(Vector2String(E.EigenValues))
+	fmt.Print("\n----------------------\n")
 }
